8afc68afdb14b247588a4ebbbc12df0661240350
Aurélien Geron <ageron@users.noreply.github.com>
2024-06-29 11:29:49 +1200

    charValue doesn't need to be at the top level

--- Day01.hs
+++ Day01.hs
@@ -2,13 +2,12 @@ module Day01 where
 
 import Data.List
 
-charValue :: Char -> Int
-charValue '(' = 1
-charValue ')' = -1
-charValue _ = 0
-
 floors :: String -> [Int]
 floors input = scanl (+) 0 $ map charValue input
+  where
+    charValue '(' = 1
+    charValue ')' = -1
+    charValue _ = 0
 
 part1 :: String -> String
 part1 input = show $ last $ floors input
