b530accfd5a808c36e4f782df78b51f78f9215ed
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-01 10:10:07 +1200

    Solve day 5 using regular expressions (regex-pcre-builtin lib)

--- Day05.hs
+++ Day05.hs
@@ -1,10 +1,24 @@
 module Day05 where
 
-notImplemented :: String -> String
-notImplemented input = "Not implemented, input length = " ++ show (length input)
+import Data.Char (toLower)
+import Text.Regex.PCRE
 
 part1 :: String -> String
-part1 = notImplemented
+part1 input = show $ length $ filter niceString (lines input)
+  where
+    niceString str = hasThreeVowels str && hasDoubleLetter str && not (hasIllegal str)
+      where
+        hasThreeVowels s = countVowels s >= 3
+          where
+            isVowel ch = toLower ch `elem` "aeiou"
+            countVowels = length . filter isVowel
+        hasDoubleLetter s = s =~ "(.)\\1"
+        hasIllegal s = s =~ "ab|cd|pq|xy"
 
 part2 :: String -> String
-part2 = notImplemented
+part2 input = show $ length $ filter niceString (lines input)
+  where
+    niceString str = hasTwoPairs str && hasSandwich str
+      where
+        hasTwoPairs s = s =~ "(..).*\\1"
+        hasSandwich s = s =~ "(.).\\1"
