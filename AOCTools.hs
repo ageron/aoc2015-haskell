156fc0b260b128f05f4a6976867754d4a409b820
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-18 21:09:56 +1000

    Use getCPUTime to measure duration

--- AOCTools.hs
+++ AOCTools.hs
@@ -1,26 +1,25 @@
 module AOCTools where
 
 import           Control.DeepSeq (deepseq)
-import           Data.Time       (NominalDiffTime, diffUTCTime, getCurrentTime)
+import           System.CPUTime
 import           Text.Printf     (printf)
 
-formatDiffTime :: NominalDiffTime -> String
+formatDiffTime :: Integer -> String
 formatDiffTime diff
-  | diff < 0.001 = printf "%.0f µs" (diffd * 1000000)
-  | diff < 1 = printf "%.0f ms" (diffd * 1000)
-  | otherwise = printf "%.2f s" diffd
+  | seconds < 0.001 = printf "%.0f µs" (seconds * 1000000)
+  | seconds < 1 = printf "%.0f ms" (seconds * 1000)
+  | otherwise = printf "%.2f s" seconds
   where
-    diffd = realToFrac diff :: Double
+    seconds = (fromIntegral diff :: Double) / 1e12
 
 runPart :: (String -> String) -> Int -> String -> IO ()
 runPart partFunction index input = do
   putStr $ "  Part " ++ show index ++ ": "
-  startTime <- getCurrentTime
+  startTime <- getCPUTime
   let result = partFunction input
-  -- deepseq ensures result is actually evaluated before getCurrentTime
-  endTime <- result `deepseq` getCurrentTime
-  putStrLn
-    $ result ++ " (" ++ formatDiffTime (diffUTCTime endTime startTime) ++ ")"
+  -- deepseq ensures result is actually evaluated before getCPUTime
+  endTime <- result `deepseq` getCPUTime
+  putStrLn $ result ++ " (" ++ formatDiffTime (endTime - startTime) ++ ")"
 
 runSolution :: (Int, (String -> String, String -> String)) -> IO ()
 runSolution (day, (part1, part2)) = do
