455a37e18d7fe430d98b1f5e7be701a81da29389
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-20 23:18:48 +1000

    Solve day 8

--- Day08.hs
+++ Day08.hs
@@ -1,7 +1,16 @@
 module Day08 where
 
 part1 :: String -> String
-part1 _ = "not implemented"
+part1 input = show $ sum $ map ((+ 2) . unescapedDiff) (lines input)
+  where
+    unescapedDiff []                = 0 :: Int
+    unescapedDiff [_]               = 0
+    unescapedDiff ('\\':'\"':xs)    = 1 + unescapedDiff xs
+    unescapedDiff ('\\':'\\':xs)    = 1 + unescapedDiff xs
+    unescapedDiff ('\\':'x':_:_:xs) = 3 + unescapedDiff xs
+    unescapedDiff (_:xs)            = unescapedDiff xs
 
 part2 :: String -> String
-part2 _ = "not implemented"
+part2 input = show $ sum $ map escapedDiff (lines input)
+  where
+    escapedDiff line = 2 + length (filter (`elem` "\"\\") line)
