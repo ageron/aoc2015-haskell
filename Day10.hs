48c64fcaac95ab696a0c6f1b4d382075640f1437
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-21 19:24:25 +1000

    Solve day 10

--- Day10.hs
+++ Day10.hs
@@ -1,7 +1,17 @@
 module Day10 where
 
+import           Data.List (group)
+
+repeatLookAndSay :: Int -> String -> String
+repeatLookAndSay n input =
+  show $ length $ iterate lookAndSay (cleanInput input) !! n
+  where
+    cleanInput = filter (/= '\n')
+    lookAndSay = concatMap say . group
+    say xs = show (length xs) ++ [head xs]
+
 part1 :: String -> String
-part1 _ = "not implemented"
+part1 = repeatLookAndSay 40
 
 part2 :: String -> String
-part2 _ = "not implemented"
+part2 = repeatLookAndSay 50
