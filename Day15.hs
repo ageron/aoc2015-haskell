1fd1fdcce5c19aaaffb56b8770886cbd0052b978
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-22 17:40:11 +1000

    Solve day 15

--- Day15.hs
+++ Day15.hs
@@ -1,7 +1,47 @@
 module Day15 where
 
+import           Data.List (transpose)
+
+parseIngredients :: String -> [[Int]]
+parseIngredients input = map (parseLine . words) (lines cleanInput)
+  where
+    cleanInput = filter (not . (`elem` ",:")) input
+    parseLine [_, "capacity", capacityStr, "durability", durabilityStr, "flavor", flavorStr, "texture", textureStr, "calories", caloriesStr] =
+      map read [caloriesStr, capacityStr, durabilityStr, flavorStr, textureStr]
+    parseLine _ = error "Invalid ingredient list format"
+
+recipes :: Int -> Int -> [[Int]]
+recipes 1 numTeaspoons = [[numTeaspoons]]
+recipes numIngredients numTeaspoons =
+  [ x : xs
+  | x <- [0 .. numTeaspoons]
+  , xs <- recipes (numIngredients - 1) (numTeaspoons - x)
+  ]
+
+score :: [[Int]] -> [Int] -> Int
+score ingredients recipe = product propertySums
+  where
+    propertySums = map (notNegative . sum) (transpose totalIngredients)
+    notNegative = max 0
+    totalIngredients = zipWith multiplyIngredients recipe ingredients
+    multiplyIngredients num ingredient = map (* num) (tail ingredient)
+
+findBestScore :: Int -> String -> String
+findBestScore requiredCalories input =
+  show $ maximum $ map (score ingredients) filteredRecipes
+  where
+    filteredRecipes =
+      if requiredCalories > 0
+        then filter fixedCalories possibleRecipes
+        else possibleRecipes
+    possibleRecipes = recipes (length ingredients) 100
+    ingredients = parseIngredients input
+    fixedCalories recipe = recipeCalories recipe == requiredCalories
+    ingredientCalories = map head ingredients
+    recipeCalories recipe = sum $ zipWith (*) recipe ingredientCalories
+
 part1 :: String -> String
-part1 _ = "not implemented"
+part1 = findBestScore 0
 
 part2 :: String -> String
-part2 _ = "not implemented"
+part2 = findBestScore 500
