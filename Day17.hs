34938842d824eee431b78f5864d00a112dee2a56
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-22 20:35:24 +1000

    Solve day 17

--- Day17.hs
+++ Day17.hs
@@ -1,7 +1,21 @@
 module Day17 where
 
+parseContainers :: String -> [Int]
+parseContainers input = map read (lines input)
+
+combinations :: Int -> [Int] -> [[Int]]
+combinations _ [] = []
+combinations 0 _ = [[]]
+combinations target [x] = [[x] | target == x]
+combinations target (x:xs) =
+  map (x :) (combinations (target - x) xs) ++ combinations target xs
+
 part1 :: String -> String
-part1 _ = "not implemented"
+part1 input = show $ length $ combinations 150 (parseContainers input)
 
 part2 :: String -> String
-part2 _ = "not implemented"
+part2 input = show $ length $ filter isShortest allCombinations
+  where
+    allCombinations = combinations 150 (parseContainers input)
+    isShortest xs = length xs == minLength
+    minLength = minimum $ map length allCombinations
