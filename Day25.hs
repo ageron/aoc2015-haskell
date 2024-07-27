d7a677865a51ee3522e710fe09372bc592130ea8
Aurélien Geron <ageron@users.noreply.github.com>
2024-07-27 14:48:06 +1000

    Solve day 25

--- Day25.hs
+++ Day25.hs
@@ -1,7 +1,21 @@
 module Day25 where
 
+parseCoordinates :: String -> (Int, Int)
+parseCoordinates = toPair . map read . words . filter (`elem` "0123456789 ")
+  where
+    toPair [row, col] = (row, col)
+    toPair _          = error "Invalid coordinates format"
+
+codeByIndex :: Int -> Int
+codeByIndex 1 = 20151125
+codeByIndex n = (codeByIndex (n - 1) * 252533) `mod` 33554393
+
+index :: (Int, Int) -> Int
+index (1, col)   = col * (col + 1) `div` 2 -- 1 + 2 + ... + col
+index (row, col) = index (1, col + row - 1) - (row - 1)
+
 part1 :: String -> String
-part1 _ = "not implemented"
+part1 = show . codeByIndex . index . parseCoordinates
 
 part2 :: String -> String
-part2 _ = "not implemented"
+part2 _ = "The end"
